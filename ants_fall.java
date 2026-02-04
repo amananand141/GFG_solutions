55fffc5a6569f49c7354723bd2df4f41805aba04
Aman Anand <142965409+amananand141@users.noreply.github.com>
2026-02-04 21:58:38 +0530

    Create ants_fall.java

---
+++ ants_fall.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getLastMoment(int n, int left[], int right[]) {
+        // code here
+        int maxLeft = 0;
+        for(int pos : left){
+            maxLeft = Math.max(maxLeft, pos);
+        }
+        
+        int maxRight = 0;
+        for(int pos : right){
+            maxRight = Math.max(maxRight, n-pos);
+        }
+        
+        return Math.max(maxRight, maxLeft);
+    }
+}
