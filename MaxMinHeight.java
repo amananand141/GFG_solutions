e136940c46d8b306b69e74da85694ebfc2ee59fb
Aman Anand <142965409+amananand141@users.noreply.github.com>
2026-02-12 16:06:22 +0530

    Create MaxMinHeight.java

---
+++ MaxMinHeight.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int maxMinHeight(int[] arr, int k, int w) {
+        // code here
+        int n = arr.length; 
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+        
+        for(int height : arr){
+            low = Math.min(low, height);
+            high = Math.max(high, height);
+        }
+        
+        high = high + k;
+        int ans = low;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            if(isPossible(arr, n, k, w, mid)){
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    public static boolean isPossible(int[] arr, int n, int k, int w, int target){
+        int[] diff = new int[n+1];
+        long used = 0;
+        int add = 0;
+        
+        for(int i=0; i<n; i++){
+            add += diff[i];
+            int currHeight = arr[i]+add;
+            
+            if(currHeight < target){
+                int need = target-currHeight;
+                used += need;
+                if(used > k)return false;
+                
+                add += need;
+                if(i+w < n){
+                    diff[i+w] -= need;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
